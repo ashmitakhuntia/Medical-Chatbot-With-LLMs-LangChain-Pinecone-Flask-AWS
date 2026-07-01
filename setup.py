b8fbf3c331de1379a50a42029695f8721fd2c7d4
Ashmita Khuntia <ashmitakhuntia10@gmail.com>
2026-07-01 23:59:08 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medicl-chatbot",
+    version="0.1.0",
+    author="Ashmita Khuntia",
+    author_email="ashmitakhuntia10@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
